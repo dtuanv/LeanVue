e52e208a75fdefe27607951afe88d052514b3397
v.dao <dtuanlieb@gmail.com>
2022-08-01 16:40:21 +0200

    create a Table in Customer Page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,13 +2,24 @@
   <q-page padding>
     <!-- content -->
     <div class="text-h5 flex flex-center">Login</div>
-    <q-ajax-bar/>
+    <q-ajax-bar />
     <q-btn label="Test"> </q-btn>
+    <pre>{{ selected }}</pre>
+
+
   </q-page>
+  <!-- <div class="q-mt-md">Selected: {{ JSON.stringify(selected) }}</div> -->
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   // name: 'PageName',
-}
+  setup() {
+    const selected = ref();
+    return {
+      selected,
+    };
+  },
+};
 </script>
